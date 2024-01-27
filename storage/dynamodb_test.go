9c60f04059fcdaf9b476664ee4f4349a262da3fc
Bartek Muszynski <220bartek@gmail.com>
2024-01-27 09:58:26 -0500

    Added a test table - update unit tests

--- storage/dynamodb_test.go
+++ storage/dynamodb_test.go
@@ -8,11 +8,11 @@ import (
 
 func TestDynamoDB(t *testing.T) {
 	// Set up
-	dd := NewDynamoDB()
+	dd := NewDynamoDB("high_throughput_test.snippets")
 	tableNames, err := dd.listTables()
 
 	if err != nil {
 		require.Fail(t, err.Error())
 	}
-	require.Equal(t, []string{"high_throughput.snippets"}, tableNames)
+	require.Equal(t, []string{"high_throughput.snippets", "high_throughput_test.snippets"}, tableNames)
 }
